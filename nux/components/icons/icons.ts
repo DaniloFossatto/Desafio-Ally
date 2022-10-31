ef38989260f3163271edf6f9ae57913ec6ea2608
Danilo Tavares Fossatto <96136272+DaniloFossatto@users.noreply.github.com>
2022-10-30 21:04:45 -0300

    Delete nux directory

--- nux/components/icons/icons.ts
+++
@@ -1,5 +0,0 @@
-export {Sketch} from '@styled-icons/boxicons-logos/Sketch'
-export {User} from '@styled-icons/boxicons-regular/User'
-export {LoginCircle} from '@styled-icons/remix-line/LoginCircle'
-export {ArrowheadDownOutline} from "@styled-icons/evaicons-outline/ArrowheadDownOutline"
-export {Profile} from '@styled-icons/icomoon/Profile'
